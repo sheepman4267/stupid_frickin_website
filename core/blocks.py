56d66755c6891dbc29a9999944321c82eecb162d
Hans K <hans@communitycomputing.net>
2024-11-13 23:57:02 -0500

    Fix formatting

--- core/blocks.py
+++ core/blocks.py
@@ -3,5 +3,6 @@ from wagtail.blocks import StructBlock, PageChooserBlock
 
 class FeaturedPageBlock(StructBlock):
     featured_page = PageChooserBlock()
+
     class Meta:
         template = 'core/blocks/featured_page.html'
