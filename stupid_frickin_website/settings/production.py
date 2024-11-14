f0b03983fd466a4b048be3edf28f27cd3dea54ba
Hans K <hans@communitycomputing.net>
2024-11-14 00:59:04 -0500

    Fix production settings

--- stupid_frickin_website/settings/production.py
+++ stupid_frickin_website/settings/production.py
@@ -3,6 +3,12 @@ from .base import *
 DEBUG = False
 
 try:
-    from .local import *
+    from base import *
 except ImportError:
     pass
+
+DEBUG = False
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+ALLOWED_HOSTS = ['stupid-frickin-website.fly.dev']
+CSRF_TRUSTED_ORIGINS = ['stupid-frickin-website.fly.dev']
